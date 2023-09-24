418e0503f732c7e0f43879958b9510cad4b9a3af
Farwa Miraj <farwamiraj0152@gmail.com>
2023-09-25 02:35:25 +0500

    deploy commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  constructor(private meta :Meta) {}
 
+  ngOnInit() {
+    this.meta.updateTag({ property: 'og:description', content: 'This is an article about Angular Meta service' });
+    this.meta.updateTag({ property: 'og:title', content: 'yaaah' });
+    this.meta.updateTag({ property: 'og:url', content: '/home' });
+    this.meta.updateTag({ property: 'og:type', content: 'website' });
+    this.meta.updateTag({ property: 'og:image', content: 'https://www.pakainfo.com/wp-content/uploads/2021/09/image-url-for-testing.jpg' });
+    
+  }
 }
